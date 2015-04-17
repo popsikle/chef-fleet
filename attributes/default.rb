c90f344ddad644806a0b6ec35cc44c92f6fa67d6
Sal Poliandro <popsikle@gmail.com>
2015-04-17 18:03:36 -0400

    updated fleet version

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,8 +7,8 @@
 
 
 default['fleet']['install_type'] = 'binary'
-default['fleet']['version'] = '0.9.1'
-default['fleet']['checksum'] = '1b9de8f740fc770ba52aea3d050460889560d02dfc1138e9702da2e1c763e2fe'
+default['fleet']['version'] = '0.9.2'
+default['fleet']['checksum'] = 'e28bf7d9fcc22cdfde0c7c8f31e3648a4847e7bda9cb69f309f24257eee3dd41'
 
 
 default['fleet']['sha256'] = '2d5c6bb0751ad3a50c0a219158439071a59e549f'
